4879a895131dc1163b74017af26a9fff3c68fad0
raomaheshwar <raomaheshwar529@gmail.com>
2023-10-04 19:06:56 +0530

    updated app.js and created neccessary folders and files for backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const {mongoose} = require('mongoose');
+const cookieParser = require('cookie-parser');
+const app = express();
+//database connnection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connected to DB"))
+.catch((err) => console.log("Not Connected to DB", err))
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+app.use('/', require('./routes/authRoutes'));
+const port = 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
